4ca920a440159350882d5c417d3740caec8e9a6e
Rachel Sipes <rasipes@gmail.com>
2020-01-19 13:57:43 -0500

    added psudocode to logic.js and submit button to index.html

--- assets/script/logic.js
+++ assets/script/logic.js
@@ -1,5 +1,5 @@
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
+// Your web app's Firebase configuration
+var firebaseConfig = {
     apiKey: "AIzaSyBzvMUOK6nXRyuB1LK2Dwwul2-rhfhWhs4",
     authDomain: "bootcamp-first-project-dc48a.firebaseapp.com",
     databaseURL: "https://bootcamp-first-project-dc48a.firebaseio.com",
@@ -7,8 +7,13 @@
     storageBucket: "bootcamp-first-project-dc48a.appspot.com",
     messagingSenderId: "551321302298",
     appId: "1:551321302298:web:8e1726a2abf245bec0a8a3"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  
+// user inputs train information
+// next arrival time is calculated using moment.js
+// minutes away is calculated using moment.js
+// train information is sent to firebase
+// train information is added to table from firebase
+
