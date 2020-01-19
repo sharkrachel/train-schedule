1d37cce5ddd64b6c251f1a6e305b2ce8ad485f4c
Rachel Sipes <rasipes@gmail.com>
2020-01-19 13:31:05 -0500

    added firebase API key and started index.html structure

---
+++ assets/script/logic.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBzvMUOK6nXRyuB1LK2Dwwul2-rhfhWhs4",
+    authDomain: "bootcamp-first-project-dc48a.firebaseapp.com",
+    databaseURL: "https://bootcamp-first-project-dc48a.firebaseio.com",
+    projectId: "bootcamp-first-project-dc48a",
+    storageBucket: "bootcamp-first-project-dc48a.appspot.com",
+    messagingSenderId: "551321302298",
+    appId: "1:551321302298:web:8e1726a2abf245bec0a8a3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
